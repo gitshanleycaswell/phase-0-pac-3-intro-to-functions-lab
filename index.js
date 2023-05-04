c168b5899616170c9810c69a5e7eb46419edf210
Shanley Caswell <shanleycaswell@gmail.com>
2023-05-03 23:31:03 -0400

    functions lab

---
+++ index.js
@@ -0,0 +1,28 @@
+function shout(string) {
+    return string.toUpperCase()
+}
+
+function whisper(string) {
+    return string.toLowerCase()
+}
+
+function logShout(string) {
+    console.log(string.toUpperCase())
+}
+logShout("string")
+
+function logWhisper(string) {
+    console.log(string.toLowerCase())
+}
+logWhisper("string")
+
+
+
+function sayHiToHeadphonedRoommate(string) {
+    if (string.toLowerCase() === string)
+        return ("I can\'t hear you!")
+    if (string.toUpperCase() === string)
+        return ("YES INDEED!")
+    if (string === "Let\'s have dinner together!")
+        return ("I would love to!")
+}
